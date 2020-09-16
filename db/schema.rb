c3522d6d231f31ccabb56602115af6c549b1f56c
tomohiro441 <metype441wetype@gmail.com>
2020-09-17 05:30:53 +0900

    usersテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -20,8 +20,15 @@ ActiveRecord::Schema.define(version: 2020_09_16_201131) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name_reading", default: "", null: false
+    t.string "first_name_reading", default: "", null: false
+    t.date "birthday", null: false
+    t.string "learning", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
