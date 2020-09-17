be2c805656f1023130e79486830f4994cda00df3
tomohiro441 <metype441wetype@gmail.com>
2020-09-18 08:29:42 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+    validates :email, :email, { format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false } }
+    validates :password, format: { with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/ }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
+  end      
 end
